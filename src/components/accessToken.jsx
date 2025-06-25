62143ab469261dd531594a7b9b6a49da4063fdce
bzcasper <bobby@aitoolpool.com>
2025-06-25 04:53:25 -0700

    refactor: remove authentication-related components and API routes

--- src/components/accessToken.jsx
+++
@@ -1,8 +0,0 @@
-import { useSession } from "next-auth/react";
-
-export default function Component() {
-  const { data } = useSession();
-  const accessToken = data?.accessToken;
-
-  return <div>Access Token: {accessToken ?? "Not available"}</div>;
-}
